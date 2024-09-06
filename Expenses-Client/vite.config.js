1c38a2e1284c67f4a0463ea74dc72bbf4f42d1ea
Ben Shuan <benshuan@gmail.com>
2024-09-06 05:22:09 +0300

    .

--- Expenses-Client/vite.config.js
+++ Expenses-Client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Expenses-App/Expenses-Client",
+  base: "/Expenses-App",
   plugins: [react()],
 })
