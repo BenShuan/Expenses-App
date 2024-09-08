1c6dc66e3ada8347cab0a65cdd41c654ab72a528
Ben Shuan <benshuan@gmail.com>
2024-09-08 07:22:37 +0300

    Updates

---
+++ assets/Button-DGWeY1Gx.js
@@ -0,0 +1,2 @@
+import{j as s}from"./index-TI4rkfow.js";const b=({type:e,onClick:r=()=>{},children:t,color:o="default"})=>{const d={default:" bg-slate-700 hover:border-slate-700 hover:text-slate-700 hover:bg-slate-200 active:bg-slate-300",alert:" bg-red-700 hover:border-red-700 hover:text-red-700 hover:bg-red-200 active:bg-red-300"};return s.jsx("button",{type:e,onClick:r,className:` py-3 px-5 text-white border-2 border-solid border-white rounded-xl
+     `+d[o],children:t})};export{b as B};
